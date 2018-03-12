3f9c6ace546559aab74d294d40dfb54aea881b35
Lilith Tundrus <echofox123@gmail.com>
2018-03-11 23:25:16 -0400

    testing info log level, need to figure out how to split the logging up when > 1 string to log

--- logger-test.ts
+++ logger-test.ts
@@ -2,8 +2,10 @@
 import Logger from './logger';
 import { logLevels } from './enuns';
 
-let logger = new Logger('./logs', logLevels.debug, false)
+let logger = new Logger('./logs', logLevels.info, true)
 
 
 logger.debug(`BEH ${5 + 1}`)
 logger.debug('beh', 'BEH')
+
+logger.info('THIS IS INFORMATION', 'THIS IS ALSO INFO')
