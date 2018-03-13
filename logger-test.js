483a8e94fea00cbb8c1fb301886a8e3027b48585
Lilith Tundrus <echofox123@gmail.com>
2018-03-12 21:05:42 -0400

    I hope this fixed it

---
+++ logger-test.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+var logger_1 = require("./logger");
+var enuns_1 = require("./enuns");
+var logger = new logger_1["default"]('./logs', enuns_1.logLevels.error, true);
+// Template literals are supported
+logger.debug("Test");
+// So are indefinite arguments (rest params)
+logger.debug('beh', 'BEH');
+logger.info('THIS IS INFORMATION', 'THIS IS ALSO INFO');
+logger.db('DB ACCESS!');
+logger.db("Accessed at " + new Date().toTimeString(), 'AAAAA');
+logger.auth('A person did a thing!');
+logger.auth('a person did ANOTHER thing', 'And this too!');
+logger.warn('Bad thing ooh!');
+logger.warn('More bad', 'Oh no!');
+logger.error('Very bad!!');
+logger.error(Error('Oh no!!!!'), new Error('ALSO VERY BAD'));
